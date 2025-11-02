f41db813307b22de0232413b8e5326f745a1df04
nicolas mezieres <nicolas.mezieres73@gmail.com>
2025-11-02 03:01:30 +0100

    Create components footer, logo, off rule class empty

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,4 @@
 import { Routes } from '@angular/router';
+import { Presentation } from './page/presentation/presentation';
 
-export const routes: Routes = [];
+export const routes: Routes = [{ path: '', component: Presentation }];
---
+++ src/app/component/footer/footer.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  imports: [],
+  template: `<footer
+    class="bg-black text-white text-2xl flex justify-center gap-10 font-Agdasima"
+  >
+    <p>Mention légale</p>
+    <p>© Copyright 2025</p>
+  </footer> `,
+})
+export class Footer {}
