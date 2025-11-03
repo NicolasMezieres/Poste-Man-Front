c8bbfd765f45477c5abede0e9893d976850144d4
nicolas mezieres <nicolas.mezieres73@gmail.com>
2025-11-03 12:36:35 +0100

    generate auth page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,8 @@
 import { Routes } from '@angular/router';
 import { Presentation } from './page/presentation/presentation';
+import { AuthComponent } from './page/auth/auth';
 
-export const routes: Routes = [{ path: '', component: Presentation }];
+export const routes: Routes = [
+  { path: '', component: Presentation },
+  { path: 'auth', component: AuthComponent },
+];
