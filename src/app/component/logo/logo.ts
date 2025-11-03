8aee72a7fd52ea4c15da6fafadb077671d7f3413
nicolas mezieres <nicolas.mezieres73@gmail.com>
2025-11-03 11:53:49 +0100

    rectify semantique

--- src/app/component/logo/logo.ts
+++ src/app/component/logo/logo.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
   selector: 'app-logo',
   imports: [],
   template: `<div class="mt-[5px] flex flex-col items-center">
-    <img src="assets/posteman.webp" />
+    <img src="assets/posteman.webp" alt="logo" />
     <h1 class="font-Alumni text-2xl md:text-5xl">POSTE MAN</h1>
   </div> `,
 })
