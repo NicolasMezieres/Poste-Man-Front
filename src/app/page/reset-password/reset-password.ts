0f89602bb36bdd900e096456a4dc4991f45a4968
nicolas mezieres <nicolas.mezieres73@gmail.com>
2025-11-17 15:54:30 +0100

    add path router page reset password, add message error field

--- src/app/page/reset-password/reset-password.ts
+++ src/app/page/reset-password/reset-password.ts
@@ -2,16 +2,30 @@ import { Component, model } from '@angular/core';
 import { Logo } from 'src/app/component/logo/logo';
 import { Footer } from 'src/app/component/footer/footer';
 import { InputFormComponent } from 'src/app/component/input/input-form/input-form';
-import { FormControl, FormGroup, Validators, ɵInternalFormsSharedModule, ReactiveFormsModule } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  Validators,
+  ɵInternalFormsSharedModule,
+  ReactiveFormsModule,
+} from '@angular/forms';
 import { matchPasswords, passwordValidator } from 'src/app/utils/function';
+import { ErrorMessage } from "src/app/component/error-message/error-message";
 
 @Component({
   selector: 'app-reset-password',
-  imports: [Logo, Footer, InputFormComponent, ɵInternalFormsSharedModule, ReactiveFormsModule],
+  imports: [
+    Logo,
+    Footer,
+    InputFormComponent,
+    ɵInternalFormsSharedModule,
+    ReactiveFormsModule,
+    ErrorMessage
+],
   templateUrl: './reset-password.html',
   styleUrl: './reset-password.css',
 })
-export class ResetPassword {
+export class ResetPasswordComponent {
   isSubmit = model<boolean>(false);
   formResetPassword = new FormGroup(
     {
@@ -34,6 +48,7 @@ export class ResetPassword {
     e.preventDefault();
     this.isSubmit.update(() => true);
     if (this.formResetPassword.valid) {
+      return;
     }
   }
 }
