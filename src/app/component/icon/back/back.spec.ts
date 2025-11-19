502c47afcd3f4ef216c10be3ee412fb7de3e194e
nicolas mezieres <nicolas.mezieres73@gmail.com>
2025-11-19 15:43:28 +0100

    test unit iconBackComponent

--- src/app/component/icon/back/back.spec.ts
+++ src/app/component/icon/back/back.spec.ts
@@ -1,22 +1,31 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { IconBackComponent } from './back';
+import { Location } from '@angular/common';
 
 describe('IconBackComponent', () => {
   let component: IconBackComponent;
   let fixture: ComponentFixture<IconBackComponent>;
-
+  let location: Location;
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [IconBackComponent],
     }).compileComponents();
 
     fixture = TestBed.createComponent(IconBackComponent);
+    location = TestBed.inject(Location);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+  describe('Function backk To The Future', () => {
+    it('Should back to previous page', () => {
+      jest.spyOn(location, 'back').mockReturnValue();
+      component.backToTheFuture();
+      expect(location.back).toHaveBeenCalled();
+    });
   });
 });
