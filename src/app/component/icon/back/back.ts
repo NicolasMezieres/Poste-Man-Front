3f50d3f716fb93cf5e2ba3542945aadbd7658366
nicolas mezieres <nicolas.mezieres73@gmail.com>
2025-11-19 15:29:49 +0100

    component icon back

--- src/app/component/icon/back/back.ts
+++ src/app/component/icon/back/back.ts
@@ -1,9 +1,17 @@
-import { Component } from '@angular/core';
+import { Location } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
-  selector: 'app-back',
-  imports: [],
-  templateUrl: './back.html',
-  styleUrl: './back.css',
+  selector: 'app-icon-back',
+  imports: [MatIcon],
+  template: `<button (click)="backToTheFuture()">
+    <mat-icon fontIcon="arrow_back" />
+  </button>`,
 })
-export class BackComponent {}
+export class BackComponent {
+  #location = inject(Location);
+  backToTheFuture() {
+    this.#location.back();
+  }
+}
--- src/app/page/profil/profil.ts
+++ src/app/page/profil/profil.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { SideBarComponent } from "src/app/component/side-bar/side-bar";
 import { MatIcon } from "@angular/material/icon";
+import { BackComponent } from "src/app/component/icon/back/back";
 
 @Component({
   selector: 'app-profil',
-  imports: [SideBarComponent, MatIcon],
+  imports: [SideBarComponent, MatIcon, BackComponent],
   templateUrl: './profil.html',
   styleUrl: './profil.css',
 })
