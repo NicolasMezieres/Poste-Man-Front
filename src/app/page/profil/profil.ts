36ddc4701cbaa27c88be4a090b7014c102cc939f
nicolas mezieres <nicolas.mezieres73@gmail.com>
2025-11-19 15:15:44 +0100

    side bar page profil

--- src/app/page/profil/profil.ts
+++ src/app/page/profil/profil.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { SideBarComponent } from "src/app/component/side-bar/side-bar";
+import { MatIcon } from "@angular/material/icon";
 
 @Component({
   selector: 'app-profil',
-  imports: [],
+  imports: [SideBarComponent, MatIcon],
   templateUrl: './profil.html',
   styleUrl: './profil.css',
 })
