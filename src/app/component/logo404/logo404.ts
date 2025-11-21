0bbd4e2c47b9950da3f5cf3768f77e4908b23aac
Daorse <mmorpgplays5@gmail.com>
2025-11-21 03:52:28 +0100

    Logo404 responsive page 404/500 responsive

--- src/app/component/logo404/logo404.ts
+++ src/app/component/logo404/logo404.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
   selector: 'app-logo404',
   imports: [],
   template: `<div class="mt-[5px] flex flex-col items-center">
-    <img src="assets/Erreur404.png" alt="logo" />
+    <img src="assets/Erreur404.png" alt="logo" class="md:w-[80%] lg:w-[50%]" />
   </div>`,
 })
 export class Logo404 {}
--- src/app/page/erreur500/erreur500.ts
+++ src/app/page/erreur500/erreur500.ts
@@ -1,35 +1,27 @@
-import { Component, inject } from '@angular/core';
+import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
-import { MatDialog } from '@angular/material/dialog';
-import { Router } from '@angular/router';
+import { RouterLink } from '@angular/router';
 import { Logo } from 'src/app/component/logo/logo';
 
 @Component({
   selector: 'app-erreur500',
-  imports: [Logo, MatButtonModule],
-  template: `<div class="flex flex-col text-center h-screen">
+  imports: [Logo, MatButtonModule, RouterLink],
+  template: `<div class="flex flex-col text-center h-screen md:gap-10">
     <app-logo></app-logo>
     <div
-      class="flex flex-col items-center justify-center gap-4 text-[32px] font-Julius mt-2"
+      class="flex flex-col items-center justify-center gap-4 text-[32px] font-Julius mt-2 md:gap-10"
     >
       <p>Une erreur et survenu.</p>
       <p>DÉSOLER POUR LE DÉRANGEMENT OCCASIONNER.</p>
       <p>VEUILLEZ RÉESSAYER PLUS TARD</p>
       <button
         matButton
-        (click)="navigate()"
-        class="border w-[280px] h-[60px] text-[24px] rounded-[10px] bg-[#7C3DD4] text-white font-Agdasima"
+        routerLink="/"
+        class="!border !w-[280px] !h-[60px] !text-[24px] !rounded-[10px] !bg-[#7C3DD4] !text-white !font-Agdasima"
       >
         Retour à l'accueil
       </button>
     </div>
   </div>`,
 })
-export class Erreur500 {
-  private router = inject(Router);
-  readonly dialog = inject(MatDialog);
-  navigate() {
-    this.dialog.closeAll();
-    this.router.navigate(['/']);
-  }
-}
+export class Erreur500 {}
