69a8be1cb24374d6d60907dd6b4a538264aff1fd
nicolas mezieres <nicolas.mezieres73@gmail.com>
2025-11-26 16:54:01 +0100

    create component button action

---
+++ src/app/component/button/button-action/button-action.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ButtonActionComponent } from './button-action';
+
+describe('ButtonActionComponent', () => {
+  let component: ButtonActionComponent;
+  let fixture: ComponentFixture<ButtonActionComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [ButtonActionComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(ButtonActionComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/component/button/button-action/button-action.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-button-action',
+  imports: [],
+  templateUrl: './button-action.html',
+  styleUrl: './button-action.css',
+})
+export class ButtonActionComponent {
+
+}
