b3997429af03bf25e8f097f52b4752c761d040fd
nicolas mezieres <nicolas.mezieres73@gmail.com>
2025-11-28 03:13:48 +0100

    responsive page tchat

--- src/app/component/header/header-project-mobile/header-project-mobile.ts
+++ src/app/component/header/header-project-mobile/header-project-mobile.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, input, output } from '@angular/core';
 import { IconBackComponent } from '../../icon/back/back';
-import { MatIcon } from "@angular/material/icon";
+import { IconMoreComponent } from '../../icon/more/more';
 
 @Component({
   selector: 'app-header-project-mobile',
-  imports: [IconBackComponent, MatIcon],
+  imports: [IconBackComponent, IconMoreComponent],
   templateUrl: './header-project-mobile.html',
   styleUrl: './header-project-mobile.css',
 })
-export class HeaderProjectMobileComponent {}
+export class HeaderProjectMobileComponent {
+  title = input.required<string>();
+  actionMore = output<void>();
+}
--- src/app/page/tchat/tchat.ts
+++ src/app/page/tchat/tchat.ts
@@ -1,15 +1,16 @@
-import { Component } from '@angular/core';
-import { IconBackComponent } from 'src/app/component/icon/back/back';
+import { Component, inject } from '@angular/core';
+import { HeaderProjectMobileComponent } from 'src/app/component/header/header-project-mobile/header-project-mobile';
 import { IconMoreComponent } from 'src/app/component/icon/more/more';
+import { MatIcon } from '@angular/material/icon';
+import { ToastService } from 'src/app/services/toast/toast';
+import { SideBarComponent } from "src/app/component/side-bar/side-bar";
 
 @Component({
   selector: 'app-tchat',
-  imports: [IconBackComponent, IconMoreComponent],
+  imports: [HeaderProjectMobileComponent, IconMoreComponent, MatIcon, SideBarComponent],
   templateUrl: './tchat.html',
   styleUrl: './tchat.css',
 })
 export class TchatComponent {
-  coucou() {
-    console.log('coucou');
-  }
+  #toast = inject(ToastService);
 }
