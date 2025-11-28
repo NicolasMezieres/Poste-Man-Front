ab1a81cb82053217a12343d9293dc1fe6ef37376
nicolas mezieres <nicolas.mezieres73@gmail.com>
2025-11-28 11:05:54 +0100

    responsive page tchat

--- src/app/page/tchat/tchat.ts
+++ src/app/page/tchat/tchat.ts
@@ -4,10 +4,11 @@ import { IconMoreComponent } from 'src/app/component/icon/more/more';
 import { MatIcon } from '@angular/material/icon';
 import { ToastService } from 'src/app/services/toast/toast';
 import { SideBarComponent } from "src/app/component/side-bar/side-bar";
+import { IconBackComponent } from "src/app/component/icon/back/back";
 
 @Component({
   selector: 'app-tchat',
-  imports: [HeaderProjectMobileComponent, IconMoreComponent, MatIcon, SideBarComponent],
+  imports: [HeaderProjectMobileComponent, IconMoreComponent, MatIcon, SideBarComponent, IconBackComponent],
   templateUrl: './tchat.html',
   styleUrl: './tchat.css',
 })
