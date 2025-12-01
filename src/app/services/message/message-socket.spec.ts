80e74651a9811779820732e43fd4356556316789
nicolas mezieres <nicolas.mezieres73@gmail.com>
2025-12-01 16:49:35 +0100

    install dependencies for test unit socket : mocha chai @types/mocha @types/chai

--- src/app/services/message/message-socket.spec.ts
+++ src/app/services/message/message-socket.spec.ts
@@ -1,9 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 import { MessageSocketService } from './message-socket';
-
+import { createServer } from 'node:http';
+import { type AddressInfo } from 'node:net';
+import { io as ioc, type Socket as ClientSocket } from 'socket.io-client';
+import { Server, type Socket as ServerSocket } from 'socket.io';
+import { assert } from 'chai';
 describe('MessageSocketService', () => {
   let service: MessageSocketService;
-
+  let io: Server, serverSocket: ServerSocket, clientSocket: ClientSocket;
   beforeEach(() => {
     TestBed.configureTestingModule({});
     service = TestBed.inject(MessageSocketService);
@@ -11,5 +15,8 @@ describe('MessageSocketService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+  describe('Function listen to exception', () => {
+    it('Should received an error and disconnect communication', () => {});
   });
 });
