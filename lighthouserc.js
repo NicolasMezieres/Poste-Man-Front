968d77553b2135fcf2c9e33623d390ed38b1b78b
nicolas mezieres <nicolas.mezieres73@gmail.com>
2025-12-05 16:42:45 +0100

    add all url

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,7 +1,17 @@
 export const ci = {
   collect: {
     startServerCommand: 'npm run start',
-    url: ['http://localhost:4200'],
+    url: [
+      'http://localhost:4200',
+      'http://localhost:4200/auth',
+      'http://localhost:4200/validAccount',
+      'http://localhost:4200/forgetPassword',
+      'http://localhost:4200/resetPassword/token',
+      'http://localhost:4200/home',
+      'http://localhost:4200/home/token',
+      'http://localhost:4200/profil',
+      'http://localhost:4200/project/id/tchat',
+    ],
   },
   upload: {
     target: 'temporary-public-storage',
