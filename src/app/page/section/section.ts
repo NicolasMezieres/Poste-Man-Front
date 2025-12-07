5b2bd9ace2d9043e15f3624161942a0cf2d75d73
nicolas mezieres <nicolas.mezieres73@gmail.com>
2025-12-07 15:59:57 +0100

    page section responsive

--- src/app/page/section/section.ts
+++ src/app/page/section/section.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { SideBarComponent } from 'src/app/component/side-bar/side-bar';
+import { IconBackComponent } from "src/app/component/icon/back/back";
+import { MatIcon } from "@angular/material/icon";
 
 @Component({
   selector: 'app-section',
-  imports: [],
+  imports: [SideBarComponent, IconBackComponent, MatIcon],
   templateUrl: './section.html',
   styleUrl: './section.css',
 })
-export class SectionComponent {}
+export class SectionComponent {
+  projectId = '06d0d448-b154-492e-8d15-0d4c487c496e';
+}
