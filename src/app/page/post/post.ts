ef0fa6674d513dc52780a36fed87a51df3c5bc36
nicolas mezieres <nicolas.mezieres73@gmail.com>
2025-12-08 16:02:28 +0100

    contain post page

--- src/app/page/post/post.ts
+++ src/app/page/post/post.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { IconBackComponent } from "src/app/component/icon/back/back";
+import { HeaderProjectMobileComponent } from "src/app/component/header/header-project-mobile/header-project-mobile";
+import { SideBarComponent } from "src/app/component/side-bar/side-bar";
+import { MatIcon } from "@angular/material/icon";
 
 @Component({
   selector: 'app-post',
-  imports: [IconBackComponent],
+  imports: [IconBackComponent, HeaderProjectMobileComponent, SideBarComponent, MatIcon],
   templateUrl: './post.html',
   styleUrl: './post.css',
 })
