046837e5572d9a9028e0a5e746531b7b0edca16f
nicolas mezieres <nicolas.mezieres73@gmail.com>
2025-12-08 20:48:25 +0100

    create post service, test unit post service

---
+++ src/app/services/post/post.ts
@@ -0,0 +1,91 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Observable, take } from 'rxjs';
+import {
+  formPostType,
+  resMessageType,
+  resPostType,
+  voteType,
+} from 'src/app/utils/type';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PostService {
+  #http = inject(HttpClient);
+  #url = environment.apiURL;
+
+  getPosts(sectionId: string): Observable<resPostType> {
+    return this.#http
+      .get<resPostType>(`${this.#url}post/section/${sectionId}`, {
+        withCredentials: true,
+      })
+      .pipe(take(1));
+  }
+  createPost(
+    sectionId: string,
+    data: formPostType,
+  ): Observable<resMessageType> {
+    return this.#http.post<resMessageType>(
+      `${this.#url}post/section/${sectionId}`,
+      data,
+      {
+        withCredentials: true,
+      },
+    );
+  }
+  updatePost(postId: string, data: formPostType): Observable<resMessageType> {
+    return this.#http.patch<resMessageType>(
+      `${this.#url}post/${postId}`,
+      data,
+      {
+        withCredentials: true,
+      },
+    );
+  }
+  movePost(postId: string, sectionId: string): Observable<resMessageType> {
+    return this.#http
+      .patch<resMessageType>(
+        `${this.#url}post/${postId}/move/${sectionId}`,
+        null,
+        {
+          withCredentials: true,
+        },
+      )
+      .pipe(take(1));
+  }
+  moveAllPost(
+    sectionId: string,
+    moveSectionId: string,
+  ): Observable<resMessageType> {
+    return this.#http
+      .patch<resMessageType>(
+        `${this.#url}post/section/${sectionId}/move/${moveSectionId}`,
+        null,
+        { withCredentials: true },
+      )
+      .pipe(take(1));
+  }
+  vote(postId: string, data: voteType): Observable<resMessageType> {
+    return this.#http
+      .put<resMessageType>(`${this.#url}post/${postId}/vote`, data, {
+        withCredentials: true,
+      })
+      .pipe(take(1));
+  }
+  delete(postId: string): Observable<resMessageType> {
+    return this.#http
+      .delete<resMessageType>(`${this.#url}post/${postId}`, {
+        withCredentials: true,
+      })
+      .pipe(take(1));
+  }
+  deleteAll(sectionId: string): Observable<resMessageType> {
+    return this.#http
+      .delete<resMessageType>(`${this.#url}post/section/${sectionId}`, {
+        withCredentials: true,
+      })
+      .pipe(take(1));
+  }
+}
