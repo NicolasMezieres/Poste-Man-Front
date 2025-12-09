0374997da60d95c05ad003b98f11b2ad1e80f6d0
nicolas mezieres <nicolas.mezieres73@gmail.com>
2025-12-09 22:30:09 +0100

    page scalable only on table, move post in the table

--- src/app/page/post/post.ts
+++ src/app/page/post/post.ts
@@ -30,20 +30,82 @@ export class PostComponent implements OnInit {
   posts = signal<postType[]>([]);
   projectId = signal<string>('');
   sectionId = signal<string>('');
-  ngOnInit() {
-    const paramsProject = this.#route.snapshot.paramMap.get('projectId');
-    const paramsSection = this.#route.snapshot.paramMap.get('sectionId');
-    if (!paramsProject || !paramsSection) {
-      this.#router.navigate(['home']);
-      return;
+  zoom = signal<number>(1);
+  offsetX = signal<number>(0);
+  offsetY = signal<number>(0);
+  zoomMore() {
+    if (this.zoom() >= 0.2) {
+      this.zoom.update((old) => Number((old - 0.1).toFixed(1)));
     }
-    this.#postService.getPosts(paramsSection).subscribe({
-      next: (res) => {
-        this.posts.update(() => res.data);
-        this.isAdmin.update(() => res.isAdmin);
-        this.isModerator.update(() => res.isModerator);
-        this.username.update(() => res.user);
+  }
+  zoomLess() {
+    if (this.zoom() < 1) {
+      this.zoom.update((old) => Number((old + 0.1).toFixed(1)));
+    }
+  }
+  disableZoom() {
+    window.addEventListener(
+      'wheel',
+      (e) => {
+        if (e.ctrlKey) {
+          e.preventDefault();
+        }
       },
+      { passive: false },
+    );
+  }
+  moveCard(e: DragEvent) {
+    const table = document.querySelector('#table') as HTMLElement;
+    const card = document.querySelector('#card1') as HTMLElement;
+    const boundingTable = table.getBoundingClientRect();
+    const boundingCard = card.getBoundingClientRect();
+    if (!card) return;
+    console.log(boundingTable.x, boundingTable.width);
+    const resultX =
+      (e.clientX - boundingCard.width / 2 - boundingTable.x) / this.zoom();
+    const resultY = (e.clientY - 30 - boundingTable.y) / this.zoom();
+    card.style.left =
+      resultX < 0
+        ? '0'
+        : resultX > (boundingTable.width - boundingCard.width) / this.zoom()
+          ? (boundingTable.width - boundingCard.width) / this.zoom() + 'px'
+          : resultX + 'px';
+    card.style.top =
+      resultY < 0
+        ? '0'
+        : resultY > (boundingTable.height - boundingCard.height) / this.zoom()
+          ? (boundingTable.height - boundingCard.height) / this.zoom() + 'px'
+          : resultY + 'px';
+  }
+  testZoomInDiv() {
+    const element = document.querySelector('#test') as HTMLElement;
+    if (!element) return;
+    element.addEventListener('wheel', (e: WheelEvent) => {
+      if (e.ctrlKey) {
+        if (e.deltaY >= 100) {
+          this.zoomMore();
+        } else if (e.deltaY <= -100) {
+          this.zoomLess();
+        }
+      }
     });
+  }
+  ngOnInit() {
+    this.disableZoom();
+    this.testZoomInDiv();
+    //   const paramsProject = this.#route.snapshot.paramMap.get('projectId');
+    //   const paramsSection = this.#route.snapshot.paramMap.get('sectionId');
+    //   if (!paramsProject || !paramsSection) {
+    //     this.#router.navigate(['home']);
+    //     return;
+    //   }
+    //   this.#postService.getPosts(paramsSection).subscribe({
+    //     next: (res) => {
+    //       this.posts.update(() => res.data);
+    //       this.isAdmin.update(() => res.isAdmin);
+    //       this.isModerator.update(() => res.isModerator);
+    //       this.username.update(() => res.user);
+    //     },
+    //   });
   }
 }
