18ee633c326fde3bde49f3a918f63e3266731ada
nicolas mezieres <nicolas.mezieres73@gmail.com>
2025-12-11 18:25:31 +0100

    move to the main

--- src/app/page/post/post.ts
+++ src/app/page/post/post.ts
@@ -31,10 +31,20 @@ export class PostComponent implements OnInit {
   projectId = signal<string>('');
   sectionId = signal<string>('');
   zoom = signal<number>(1);
-  offsetX = signal<number>(0);
-  offsetY = signal<number>(0);
+  originX = signal<number>(0);
+  originY = signal<number>(0);
+  isMovable = signal<boolean>(false);
+  cursorX = signal<number>(0);
+  cursorY = signal<number>(0);
+  previousScrollX = signal<number>(0);
+  previousScrollY = signal<number>(0);
+  //todo regarder si canva peut faire le zoom
+  //todo changer le minimum zoom par
+  // si main.width est plus grand que table.width alors on zoom pas
+  // si table width plus grand que que main width * (this.zoom() -0.1)
+  // && this.zoom() supérieur à 0.1 alors on zoom
   zoomMore() {
-    if (this.zoom() >= 0.2) {
+    if (this.zoom() >= 0.4) {
       this.zoom.update((old) => Number((old - 0.1).toFixed(1)));
     }
   }
@@ -43,6 +53,28 @@ export class PostComponent implements OnInit {
       this.zoom.update((old) => Number((old + 0.1).toFixed(1)));
     }
   }
+  scroll(e: MouseEvent) {
+    const main = document.querySelector('main');
+    const table = document.querySelector('#table');
+    if (!main || !table) return;
+    const boundingTable = table.getBoundingClientRect();
+    const boundingMain = main.getBoundingClientRect();
+    //je pense qu'il manque juste à trouver la taille de table sur l'écran
+    // genre 1500 étant donner qu'on trouve 500
+    // console.log(boundingMain.width, main.scrollLeft);
+    console.log(
+      e.offsetX,
+      boundingMain,
+      e.clientX,
+      main.scrollLeft,
+      this.zoom(),
+    );
+    //   boundingTable.width,
+    //   boundingTable,
+  }
+  //todo: ff le zoom;
+  //todo: par contre se déplacer avec le clic droit de la souris serait pas mal
+  //todo: essayer le déplacer en tenant une card et se déplacer  à gauche ou autre
   disableZoom() {
     window.addEventListener(
       'wheel',
@@ -56,11 +88,10 @@ export class PostComponent implements OnInit {
   }
   moveCard(e: DragEvent) {
     const table = document.querySelector('#table') as HTMLElement;
-    const card = document.querySelector('#card1') as HTMLElement;
+    const card = e.target as HTMLElement;
     const boundingTable = table.getBoundingClientRect();
     const boundingCard = card.getBoundingClientRect();
     if (!card) return;
-    console.log(boundingTable.x, boundingTable.width);
     const resultX =
       (e.clientX - boundingCard.width / 2 - boundingTable.x) / this.zoom();
     const resultY = (e.clientY - 30 - boundingTable.y) / this.zoom();
@@ -78,17 +109,88 @@ export class PostComponent implements OnInit {
           : resultY + 'px';
   }
   testZoomInDiv() {
-    const element = document.querySelector('#test') as HTMLElement;
-    if (!element) return;
-    element.addEventListener('wheel', (e: WheelEvent) => {
+    const main = document.querySelector('main');
+    const table = document.getElementById('table');
+    if (!table || !main) return;
+    main.addEventListener('wheel', (e: WheelEvent) => {
       if (e.ctrlKey) {
-        if (e.deltaY >= 100) {
-          this.zoomMore();
-        } else if (e.deltaY <= -100) {
-          this.zoomLess();
+        // if (e.deltaY >= 100) {
+        //   this.zoomMore();
+        // } else if (e.deltaY <= -100) {
+        //   this.zoomLess();
+        // }
+        if (e.deltaY) {
+          this.updateZooming(e);
         }
       }
     });
+  }
+  updateZooming(e: WheelEvent) {
+    const main = document.querySelector('main');
+    const table = document.getElementById('table');
+    if (!main || !table) return;
+    const oldScale = this.zoom();
+    const oldX = this.originX();
+    const oldY = this.originY();
+
+    const boundingMain = main.getBoundingClientRect();
+    const boundingTable = table.getBoundingClientRect();
+    const localX = e.clientX - boundingMain.left + main.scrollLeft;
+    // const localX =
+    //   calculX < boundingMain.width
+    //     ? 0
+    //     : calculX > boundingTable.width - boundingMain.width
+    //       ? boundingTable.width - boundingMain.width
+    //       : calculX;
+    // console.log(
+    //   calculX > boundingTable.width,
+    //   boundingTable.width / oldScale,
+    //   boundingMain.width,
+    //   localX,
+    // );
+    const localY = e.clientY - boundingMain.top + main.scrollTop;
+    // const localY = calculY > boundingMain.height ? calculY : 0;
+    const previousScale = this.zoom();
+    const newScale = Math.min(
+      1,
+      Math.max(0.3, this.zoom() + e.deltaY * -0.001),
+    );
+    let newX = localX - (localX - oldX) * (newScale / previousScale);
+    let newY = localY - (localY - oldY) * (newScale / previousScale);
+    const visibleWidth = main.clientWidth;
+    const visibleHeight = main.clientHeight;
+
+    const contentWidth = 5000 * newScale;
+    const contentHeight = 5000 * newScale;
+
+    const minX = visibleWidth - contentWidth;
+    let maxX = 0;
+
+    const minY = visibleHeight - contentHeight;
+    let maxY = 0;
+    newX = Math.min(0, Math.max(minX, newX));
+    newY = Math.min(0, Math.max(minY, newY));
+    this.originX.update(() => newX);
+    this.originY.update(() => newY);
+    this.zoom.update(() => newScale);
+  }
+  toggleMove(value: boolean, e: PointerEvent) {
+    this.cursorX.update(() => e.clientX);
+    this.cursorY.update(() => e.clientY);
+    this.isMovable.update(() => value);
+    const main = document.querySelector('main');
+    if (!main) return;
+    this.previousScrollX.update(() => main.scrollLeft);
+    this.previousScrollY.update(() => main.scrollTop);
+  }
+  move(e: MouseEvent) {
+    if (this.isMovable()) {
+      const main = document.querySelector('main');
+      if (!main) return;
+      console.log(main.scrollLeft, this.cursorY(), e.clientY);
+      main.scrollLeft = this.previousScrollX() + this.cursorX() - e.clientX;
+      main.scrollTop = this.previousScrollY() + this.cursorY() - e.clientY;
+    }
   }
   ngOnInit() {
     this.disableZoom();
