125acc07fda0ff63fb85aeb2401f37652388f206
nicolas mezieres <nicolas.mezieres73@gmail.com>
2025-12-15 16:55:24 +0100

    rectify verification dialog spec

--- src/app/component/modal/verification/verification.spec.ts
+++ src/app/component/modal/verification/verification.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { Verification } from './verification';
+import { MatDialogRef } from '@angular/material/dialog';
+import { dialogMock } from '../create-post/mock/dialog.mock';
 
 describe('Verification', () => {
   let component: Verification;
@@ -8,9 +10,9 @@ describe('Verification', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [Verification]
-    })
-    .compileComponents();
+      imports: [Verification],
+      providers: [{ provide: MatDialogRef, useValue: dialogMock }],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(Verification);
     component = fixture.componentInstance;
