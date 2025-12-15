1ea75dfb9afae587b121d328af3820eec7291c20
nicolas mezieres <nicolas.mezieres73@gmail.com>
2025-12-15 16:48:26 +0100

    create dialogMock, rectify  create post spec, erreur 500 spec

--- src/app/page/erreur500/erreur500.spec.ts
+++ src/app/page/erreur500/erreur500.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { Erreur500 } from './erreur500';
+import { provideRouter } from '@angular/router';
 
 describe('Erreur500', () => {
   let component: Erreur500;
@@ -8,9 +9,9 @@ describe('Erreur500', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [Erreur500]
-    })
-    .compileComponents();
+      imports: [Erreur500],
+      providers: [provideRouter([])],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(Erreur500);
     component = fixture.componentInstance;
