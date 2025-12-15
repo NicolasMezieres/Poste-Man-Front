2f21d06b0f220e80d6a6122ac86b0ab9568cc47a
nicolas mezieres <nicolas.mezieres73@gmail.com>
2025-12-15 16:50:53 +0100

    rectify confirmation dialog spec

--- src/app/component/modal/confirmation/confirmation.spec.ts
+++ src/app/component/modal/confirmation/confirmation.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { Confirmation } from './confirmation';
+import { MatDialogRef } from '@angular/material/dialog';
+import { dialogMock } from '../create-post/mock/dialog.mock';
 
 describe('Confirmation', () => {
   let component: Confirmation;
@@ -8,9 +10,9 @@ describe('Confirmation', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [Confirmation]
-    })
-    .compileComponents();
+      imports: [Confirmation],
+      providers: [{ provide: MatDialogRef, useValue: dialogMock }],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(Confirmation);
     component = fixture.componentInstance;
