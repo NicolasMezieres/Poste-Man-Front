e3cc5de62b189723e11e5de52d109badf1acd781
nicolas mezieres <nicolas.mezieres73@gmail.com>
2025-12-18 19:12:22 +0100

    test unit join project page

--- src/app/page/join-project/join-project.ts
+++ src/app/page/join-project/join-project.ts
@@ -9,7 +9,13 @@ import { HttpErrorResponseType } from 'src/app/utils/type';
 @Component({
   selector: 'app-join-project',
   imports: [MatProgressSpinner, SideBarComponent],
-  templateUrl: './join-project.html',
+  template: `<div class="h-screen w-screen bg-white flex">
+    <app-side-bar />
+    <main class="flex flex-col flex-1 justify-center items-center gap-20">
+      <h1 class="font-Julius text-[32px]">En cours de validation !</h1>
+      <mat-spinner [diameter]="150" />
+    </main>
+  </div> `,
 })
 export class JoinProjectComponent implements OnInit {
   #router = inject(Router);
