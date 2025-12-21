6b9a71498656f137dd02f272f075efed99d914eb
nicolas mezieres <nicolas.mezieres73@gmail.com>
2025-12-21 16:53:28 +0100

    unit test dialog delete section

--- src/app/component/modal/section/delete-section/delete-section.spec.ts
+++ src/app/component/modal/section/delete-section/delete-section.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DeleteSectionComponent } from './delete-section';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { dialogMock } from '../../create-post/mock/dialog.mock';
 
 describe('DeleteSectionComponent', () => {
   let component: DeleteSectionComponent;
@@ -9,6 +11,10 @@ describe('DeleteSectionComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [DeleteSectionComponent],
+      providers: [
+        { provide: MatDialogRef, useValue: dialogMock },
+        { provide: MAT_DIALOG_DATA, useValue: jest.fn() },
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(DeleteSectionComponent);
@@ -16,7 +22,18 @@ describe('DeleteSectionComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  describe('close Diaog', () => {
+    it('Should close dialog', () => {
+      component.closeDialog();
+      expect(component['dialog'].close).toHaveBeenCalledWith();
+    });
+  });
+  describe('delete Section', () => {
+    it('Should close dialog with data', () => {
+      component.deleteSection();
+      expect(component['dialog'].close).toHaveBeenCalledWith({
+        isSubmit: true,
+      });
+    });
   });
 });
