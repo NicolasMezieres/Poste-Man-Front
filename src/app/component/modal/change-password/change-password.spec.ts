9d9450bf8ee9465fa2f7dc0bf08aee9c3890d2ba
nicolas mezieres <nicolas.mezieres73@gmail.com>
2026-01-07 20:41:06 +0100

    add logout authServiceMock, unit test function close in dialogChangePasswordComponent, rectify unit test searchProject

--- src/app/component/modal/change-password/change-password.spec.ts
+++ src/app/component/modal/change-password/change-password.spec.ts
@@ -7,13 +7,11 @@ import { dialogMock } from '../dialogMock/dialog-mock';
 describe('dialogChangePasswordComponent', () => {
   let component: dialogChangePasswordComponent;
   let fixture: ComponentFixture<dialogChangePasswordComponent>;
-
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [dialogChangePasswordComponent],
       providers: [{ provide: MatDialogRef, useValue: dialogMock }],
     }).compileComponents();
-
     fixture = TestBed.createComponent(dialogChangePasswordComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -22,4 +20,12 @@ describe('dialogChangePasswordComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+  describe('close', () => {
+    it('Should close dialog', () => {
+      jest.spyOn(dialogMock, 'close');
+      component.close();
+      expect(dialogMock.close).toHaveBeenCalled();
+    });
+  });
+  describe('submit Change Password', () => {});
 });
