8fb5ba84f7291345bbdf433cbe0f093d1756b6a1
nicolas mezieres <nicolas.mezieres73@gmail.com>
2026-01-14 16:32:13 +0100

    rectify unit test error

--- src/app/services/auth/auth-socket.spec.ts
+++ src/app/services/auth/auth-socket.spec.ts
@@ -11,4 +11,20 @@ describe('AuthSocketService', () => {
     service = TestBed.inject(AuthSocketService);
     service['socket'] = socketMock;
   });
+  describe('listenToException', () => {
+    it('Should disconnect', () => {});
+  });
+  describe('authSocket', () => {
+    it('Should emit auth', () => {});
+    it('Should nothing already auth', () => {});
+  });
+  describe('connectedListMember', () => {
+    it('Should return list member', () => {});
+  });
+  describe('listenAuth', () => {
+    it('Should return when server emit online', () => {});
+    it('Should return when server emit offline', () => {});
+    it('Should return when server emit userJoinProject', () => {});
+    it('Should return when server emit userLeaveProject', () => {});
+  });
 });
