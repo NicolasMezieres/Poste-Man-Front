6040f195bffa95bc373342b5c25fd78b6967b1b2
nicolas mezieres <nicolas.mezieres73@gmail.com>
2026-03-06 18:50:38 +0100

    Rectify variable environment domain

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   apiURL: 'https://posteman-5ad0ccd49c9b.herokuapp.com/',
-  domain: 'https://poste-man-front.vercel.app',
+  domain: 'https://poste-man-front.vercel.app/',
 };
